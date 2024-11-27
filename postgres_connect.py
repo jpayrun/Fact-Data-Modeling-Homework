0c31b7da2b891d70c75eba84d7dad3338635471f
Jordan <27322284+jpayrun@users.noreply.github.com>
2024-11-27 17:32:08 -0500

    Initial checkin

---
+++ postgres_connect.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import psycopg2
+
+
+class PostgresConnection:
+
+    def __init__(self, config: dict) -> None:
+        """
+        Constructor
+
+        Args:
+            config (dict): Configuration Dictionary
+        """
+        self.config = config
+
+    def _connection(self):
+        """
+        Build the connection to Postgres Server
+
+        Returns:
+           Postgres connection
+        """
+        return psycopg2.connect(
+            database=self.config['POSTGRES_DB'],
+            user=self.config['POSTGRES_USER'],
+            password=self.config['POSTGRES_PASSWORD'],
+            port=self.config['HOST_PORT'])
+    
+    def read_sql(self, sql: str) -> pd.DataFrame:
+        """
+        Download the sql statement from postgres server
+
+        Args:
+            sql (str): Sql command
+
+        Returns:
+            pd.DataFrame: DataFrame with result
+        """
+        return pd.read_sql(sql, self._connection())
+    
+    def read_table(self, table: str) -> pd.DataFrame:
+        """
+        Return sql results as a table
+
+        Args:
+            table (str): table name to pull data for
+
+        Returns:
+            pd.DataFrame: Table as a DataFrame
+        """
+        return self.read_sql(sql=f"SELECT * from {table}")
