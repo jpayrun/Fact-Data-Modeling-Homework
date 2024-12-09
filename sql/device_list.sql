3fd18c6d4674183273639225be6e54e02a8e57ee
Jordan <27322284+jpayrun@users.noreply.github.com>
2024-12-08 21:15:32 -0500

    intial checkin

---
+++ sql/device_list.sql
@@ -0,0 +1,19 @@
+insert into date_list_int
+                with today as (
+                select 
+                    distinct host
+                from events
+                where
+                    event_time::date = '{date}')
+                
+                select
+                    t.host,
+                    case
+                        when dli.host is null then array_fill(0::bit, array[Date('{date}'::date) - Date('{first_month}'::date)]) || Array[1::bit]
+                        when t.host is null then dli.date_list_int || Array[0::bit]
+                        --when t.host is not null then dli.date_list_int || Array[1::bit]
+                        else dli.date_list_int || Array[1::bit]
+                    end as date_list_int
+                from today t
+                    full outer join date_list_int dli
+                        on t.host = dli.host
